a2364f4c0ccd7268a23bc9bb7580a7e81a9bc594
carlosa54 <carlos_arg5@yahoo.com>
2016-08-16 20:12:29 -0400

    Change admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import UserCheckout, UserAddress, Order
 # Register your models here.
+
+from django.contrib.contenttypes.models import ContentType
+
+class ContentTypeAdmin(admin.ModelAdmin):
+	list_display = ['name', 'app_label']
+	fieldsets = (
+	('', {
+		'classes': ('',),
+		'fields': ('name', 'app_label')
+	}),
+	)
+
+admin.site.register(ContentType, ContentTypeAdmin)
+
 admin.site.register(UserCheckout)
 
 admin.site.register(UserAddress)
 
 admin.site.register(Order)
+
+
 
 

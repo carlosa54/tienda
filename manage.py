6cef388556661787bfd1f87aed6a5fca65dfd649
carlosa54 <carlos_arg5@yahoo.com>
2016-08-20 17:49:17 -0400

    Changed ENVIRONMENT variable

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	if ENVIRONMENT == 'STAGING':
 		settings = 'staging'
 	elif ENVIRONMENT == 'PRODUCTION':
-		settings = 'production'
+		settings = 'local'
 	else:
 		settings = 'development'
 		

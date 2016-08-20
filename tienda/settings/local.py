9c7eaac17dd1e80519412573db6259c7591621c3
carlosa54 <carlos_arg5@yahoo.com>
2016-08-20 14:56:21 -0400

    Changed environment

--- tienda/settings/local.py
+++ tienda/settings/local.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'carlosarg54@gmail.com'
-#EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
